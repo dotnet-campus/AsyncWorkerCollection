44855ba6b8e331be828d4867283dbeb8aef4ffa9
lindexi <lindexi_gd@163.com>
2020-09-12 19:22:21 +0800

    支持.NET 4.5 版本

---
+++ AsyncWorkerCollection/ConcurrentQueueExtension.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dotnetCampus.Threading
+{
+    static class ConcurrentQueueExtension
+    {
+        public static void Clear<T>(this ConcurrentQueue<T> queue)
+        {
+            while (queue.TryDequeue(out _))
+            {
+                
+            }
+        }
+    }
+}
