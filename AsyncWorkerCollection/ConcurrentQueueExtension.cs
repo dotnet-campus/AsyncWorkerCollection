0d35e9ccee8737331c6fb08e5f1f50a9879f3662
lindexi <lindexi_gd@163.com>
2020-09-12 19:24:17 +0800

    加上注释

--- AsyncWorkerCollection/ConcurrentQueueExtension.cs
+++ AsyncWorkerCollection/ConcurrentQueueExtension.cs
@@ -9,6 +9,7 @@ namespace dotnetCampus.Threading
 {
     static class ConcurrentQueueExtension
     {
+        // 在 .NET Framework 4.5 没有清理方法
         public static void Clear<T>(this ConcurrentQueue<T> queue)
         {
             while (queue.TryDequeue(out _))
--- AsyncWorkerCollection/IAsyncDisposable.cs
+++ AsyncWorkerCollection/IAsyncDisposable.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 
 namespace dotnetCampus.Threading
 {
+    // 这个接口在 .NET Framework 4.5 没有
     interface IAsyncDisposable
     {
     }
