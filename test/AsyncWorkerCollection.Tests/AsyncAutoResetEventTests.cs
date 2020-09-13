d62a3755dc0f3bbdd4102c128c5548af79707b74
lindexi <lindexi_gd@163.com>
2020-09-13 17:20:46 +0800

    测试等待

--- test/AsyncWorkerCollection.Tests/AsyncAutoResetEventTests.cs
+++ test/AsyncWorkerCollection.Tests/AsyncAutoResetEventTests.cs
@@ -5,6 +5,7 @@ using System.Threading;
 using System.Threading.Tasks;
 using dotnetCampus.Threading;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
 using MSTest.Extensions.Contracts;
 
 namespace AsyncWorkerCollection.Tests
@@ -13,8 +14,155 @@ namespace AsyncWorkerCollection.Tests
     public class AsyncAutoResetEventTests
     {
         [ContractTestCase]
-        public void Set()
+        public void WaitForSuccessOrResult()
         {
+            "当使用 Set 次数超过 WaitOneAsync 次数，多余的 Set 只被计算一次".Test(() =>
+            {
+                Console.WriteLine("当使用 Set 次数超过 WaitOneAsync 次数，多余的 Set 只被计算一次");
+                // Arrange
+                var asyncAutoResetEvent = new AsyncAutoResetEvent(false);
+                var mock = new Mock<IFakeJob>();
+
+                // Action
+                var task1 = Task.Run(async () =>
+                {
+                    await asyncAutoResetEvent.WaitOneAsync();
+                    mock.Object.Do();
+                });
+
+                for (var i = 0; i < 5; i++)
+                {
+                    asyncAutoResetEvent.Set();
+                    // 先干掉第一次的 task 方法，测试超过了第一次的等待
+                    // 如果没有下面方法，可以认为 task1 还没创建出来，此时调用 Set 还是在没有线程等待
+                    Task.WaitAny(task1, Task.Delay(TimeSpan.FromSeconds(1)));
+                }
+
+                var taskList = new List<Task>();
+                for (var i = 0; i < 5; i++)
+                {
+                    var task = Task.Run(async () =>
+                    {
+                        Console.WriteLine("进入调用");
+                        await asyncAutoResetEvent.WaitOneAsync();
+                        mock.Object.Do();
+                    });
+                    taskList.Add(task);
+                }
+
+                foreach (var task in taskList)
+                {
+                    Task.WaitAny(task, Task.Delay(TimeSpan.FromSeconds(1)));
+                }
+
+                // Assert
+                mock.Verify(job => job.Do(), Times.Exactly(2));
+            });
+
+            "在先设置 Set 然后再 WaitOneAsync 只有一个线程执行".Test(() =>
+            {
+                // Arrange
+                var asyncAutoResetEvent = new AsyncAutoResetEvent(false);
+                var mock = new Mock<IFakeJob>();
+
+                // Action
+                asyncAutoResetEvent.Set();
+                var task1 = Task.Run(async () =>
+                {
+                    await asyncAutoResetEvent.WaitOneAsync();
+                    mock.Object.Do();
+                });
+
+                var task2 = Task.Run(async () =>
+                {
+                    await asyncAutoResetEvent.WaitOneAsync();
+                    mock.Object.Do();
+                });
+
+                Task.WaitAny(task1, task2, Task.Delay(TimeSpan.FromSeconds(1)));
+
+                // Assert
+                mock.Verify(job => job.Do(), Times.Once);
+            });
+
+            "使用 AsyncAutoResetEvent 设置一次 Set 对应一次 WaitOneAsync 的线程执行".Test(() =>
+            {
+                // Arrange
+                var asyncAutoResetEvent = new AsyncAutoResetEvent(false);
+                var mock = new Mock<IFakeJob>();
+
+                // Action
+                var taskList = new List<Task>(10);
+                for (var i = 0; i < 10; i++)
+                {
+                    var task = Task.Run(async () =>
+                    {
+                        await asyncAutoResetEvent.WaitOneAsync();
+                        mock.Object.Do();
+                    });
+                    taskList.Add(task);
+                }
+
+                // 等待 Task 都进入 await 方法
+                // 如果没有等待，可以都在线程创建上面，此时调用多次的 Set 只是做初始化
+                // 也就是当前没有线程等待，然后进行多次 Set 方法
+                foreach (var task in taskList)
+                {
+                    Task.WaitAny(task, Task.Delay(TimeSpan.FromSeconds(1)));
+                }
+
+                for (var i = 0; i < 5; i++)
+                {
+                    asyncAutoResetEvent.Set();
+                }
+
+                foreach (var task in taskList)
+                {
+                    Task.WaitAny(task, Task.Delay(TimeSpan.FromSeconds(1)));
+                }
+
+                // Assert
+                mock.Verify(job => job.Do(), Times.Exactly(5));
+            });
+
+            "构造函数设置为 true 等待 WaitOneAsync 的线程会执行".Test(() =>
+            {
+                // Arrange
+                var asyncAutoResetEvent = new AsyncAutoResetEvent(true);
+                var mock = new Mock<IFakeJob>();
+
+                // Action
+                var task = Task.Run(async () =>
+                {
+                    await asyncAutoResetEvent.WaitOneAsync();
+                    mock.Object.Do();
+                });
+
+                Task.WaitAny(task, Task.Delay(TimeSpan.FromSeconds(1)));
+
+                // Assert
+                mock.Verify(job => job.Do(), Times.Once);
+            });
+
+            "构造函数设置为 false 等待 WaitOneAsync 的线程不会执行".Test(() =>
+            {
+                // Arrange
+                var asyncAutoResetEvent = new AsyncAutoResetEvent(false);
+                var mock = new Mock<IFakeJob>();
+
+                // Action
+                var task = Task.Run(async () =>
+                {
+                    await asyncAutoResetEvent.WaitOneAsync();
+                    mock.Object.Do();
+                });
+
+                Task.WaitAny(task, Task.Delay(TimeSpan.FromSeconds(1)));
+
+                // Assert
+                mock.Verify(job => job.Do(), Times.Never);
+            });
+
             "在 WaitOne 之前调用多次 Set 只有在调用之后让一个 WaitOne 方法继续".Test(() =>
             {
                 using var asyncAutoResetEvent = new AsyncAutoResetEvent(false);
@@ -70,6 +218,11 @@ namespace AsyncWorkerCollection.Tests
                 var taskResult = task.Result;
                 Assert.AreEqual(false, taskResult);
             });
+        }
+
+        public interface IFakeJob
+        {
+            void Do();
         }
     }
 }
