b365fa976022379ffc62b13dd713cd5bd879cffc
lindexi <lindexi_gd@163.com>
2020-11-17 15:34:53 +0800

    修复还有数据进入队列之后没有被消费，调用销毁时抛出异常
    假定依然有数据没有被消费，此时调用销毁的方法里面释放了 int.MaxValue 次将会超过信号量能支持的最大次数
    
    解决方法就是只释放当前在 DequeueAsync 等待的次数

---
+++ test/AsyncWorkerCollection.Tests/AsyncQueueTest.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using dotnetCampus.Threading;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MSTest.Extensions.Contracts;
+
+namespace AsyncWorkerCollection.Tests
+{
+    [TestClass]
+    public class AsyncQueueTest
+    {
+        [ContractTestCase]
+        public void DisposeTest()
+        {
+            "在进行销毁之前，存在元素没有出队，可以成功销毁".Test(() =>
+            {
+                // Arrange
+                var asyncQueue = new AsyncQueue<int>();
+                asyncQueue.Enqueue(0);
+                asyncQueue.Enqueue(0);
+
+                // Action
+                asyncQueue.Dispose();
+
+                // Assert
+                Assert.AreEqual(0, asyncQueue.Count);
+            });
+        }
+    }
+}
