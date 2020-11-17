be552571929b4c0e193a275091e765a75e7d9dfe
lindexi <lindexi_gd@163.com>
2020-11-17 15:51:25 +0800

    加入更多单元测试内容

--- test/AsyncWorkerCollection.Tests/AsyncQueueTest.cs
+++ test/AsyncWorkerCollection.Tests/AsyncQueueTest.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Diagnostics;
+using System.Threading.Tasks;
 using dotnetCampus.Threading;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using MSTest.Extensions.Contracts;
@@ -11,6 +13,21 @@ namespace AsyncWorkerCollection.Tests
         [ContractTestCase]
         public void DisposeTest()
         {
+            "调用 AsyncQueue 销毁方法，有一个线程在等待出队，将会释放当前在等待出队的任务".Test(async () =>
+            {
+                // Arrange
+                var asyncQueue = new AsyncQueue<int>();
+                var task1 = asyncQueue.DequeueAsync();
+
+                // Action
+                asyncQueue.Dispose();
+                // 等待一下 DequeueAsync 逻辑的完成
+                await Task.Delay(TimeSpan.FromSeconds(1));
+
+                // Assert
+                Assert.AreEqual(true, task1.IsCompleted);
+            });
+
             "在 AsyncQueue 进行销毁之前，存在元素没有出队，调用销毁时可以成功销毁".Test(() =>
             {
                 // Arrange
