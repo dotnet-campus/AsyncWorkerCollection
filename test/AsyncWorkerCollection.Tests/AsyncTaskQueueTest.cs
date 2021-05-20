dfe1cafedee89531f59eacbb3ca72ce61e9070ed
lindexi <lindexi_gd@163.com>
2021-05-20 15:07:31 +0800

    在执行任务结束的时候调用 AsyncTaskQueue 销毁方法，将存在线程安全问题

--- test/AsyncWorkerCollection.Tests/AsyncTaskQueueTest.cs
+++ test/AsyncWorkerCollection.Tests/AsyncTaskQueueTest.cs
@@ -12,6 +12,29 @@ namespace AsyncWorkerCollection.Tests
         [ContractTestCase]
         public void DisposeTest()
         {
+            "在执行任务结束的时候调用 AsyncTaskQueue 销毁方法，可以不抛异常销毁".Test(async () =>
+            {
+                AsyncTaskQueue asyncTaskQueue = new AsyncTaskQueue() { AutoCancelPreviousTask = true, UseSingleThread = true };
+                var autoResetEvent = new AutoResetEvent(false);
+                _ = Task.Run(() =>
+                {
+                    autoResetEvent.WaitOne();
+                    asyncTaskQueue.Dispose();
+                });
+
+                var result = await asyncTaskQueue.ExecuteAsync(async () =>
+                {
+                    await Task.Delay(10);
+                    autoResetEvent.Set();
+                    return 1;
+                });
+
+                Thread.Sleep(2000);
+
+                Assert.AreEqual(true, result.IsInvalid);
+                Assert.AreEqual(1, result.Result);
+            });
+
             "调用 AsyncTaskQueue 销毁方法，可以不抛异常销毁".Test(async () =>
             {
                 AsyncTaskQueue asyncTaskQueue = new AsyncTaskQueue() { AutoCancelPreviousTask = true, UseSingleThread = true };
