24fe88c6ff95225ba8fc6c925c26a2be35b852fc
lindexi <lindexi_gd@163.com>
2021-09-14 20:59:57 +0800

    尝试修复边界的线程安全
    内部代号： 26255

--- test/AsyncWorkerCollection.Tests/AsyncTaskQueueTest.cs
+++ test/AsyncWorkerCollection.Tests/AsyncTaskQueueTest.cs
@@ -1,4 +1,5 @@
-﻿using System.Threading;
+﻿using System;
+using System.Threading;
 using System.Threading.Tasks;
 using dotnetCampus.Threading;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
@@ -12,6 +13,39 @@ namespace AsyncWorkerCollection.Tests
         [ContractTestCase]
         public void DisposeTest()
         {
+            "连续开始任务的时候，无法立刻释放".Test(async () =>
+            {
+                AsyncTaskQueue asyncTaskQueue = new AsyncTaskQueue() { AutoCancelPreviousTask = true, UseSingleThread = true };
+
+                var autoResetEvent = new AutoResetEvent(false);
+                _ = Task.Run(() =>
+                {
+                    autoResetEvent.WaitOne();
+                    asyncTaskQueue.Dispose();
+                });
+
+                var result = await asyncTaskQueue.ExecuteAsync(async () =>
+                {
+                    await Task.Delay(10);
+                    autoResetEvent.Set();
+                    return 1;
+                });
+
+                await Task.Delay(100);
+
+                for (int i = 0; i < 10; i++)
+                {
+                   _ = asyncTaskQueue.ExecuteAsync<bool>((Func<Task>)(() => Task.CompletedTask));
+                }
+
+                await Task.Delay(1000);
+                autoResetEvent.Set();
+
+                // 没有抛出异常就是符合预期
+                Assert.AreEqual(true, result.IsInvalid);
+                Assert.AreEqual(1, result.Result);
+            });
+
             "在执行任务结束的时候调用 AsyncTaskQueue 销毁方法，可以不抛异常销毁".Test(async () =>
             {
                 AsyncTaskQueue asyncTaskQueue = new AsyncTaskQueue() { AutoCancelPreviousTask = true, UseSingleThread = true };
